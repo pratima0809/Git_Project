f16955b1c0f2bdf90fca8e7cc1f35a4296afaafa
harshmishra21 <2024.harshm@isu.ac.in>
2025-01-25 10:35:25 +0530

    git pull

--- Cylinder.py
+++ Cylinder.py
@@ -1,2 +1,7 @@
 class Cylinder:
+    def __init__(self, height=1, radius=1):
+        self.height = height
+        self.radius = radius
+    def volume(self):
+        return self.height*3.14*self.radius(self.radius + self.height)
     pass
